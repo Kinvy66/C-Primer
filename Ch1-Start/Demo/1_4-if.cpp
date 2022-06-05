ee03309ee5b34ae0d4fba389cf6bbe4e06053295
Kinvy <kinvy66@163.com>
2022-06-05 19:41:07 +0800

    update ch1

--- Ch1-Start/Demo/1_4-if.cpp
+++ Ch1-Start/Demo/1_4-if.cpp
@@ -5,22 +5,22 @@ int main()
     int currVal = 0, val = 0;
     // 读取第一个数，并确保确实有数据可以处理
     if(std::cin >> currVal) {
-        int cnt = 1;
-        while(std::cin >> val) {
-            if(val == currVal) {
-                ++cnt;
+        int cnt = 1;                // 保证我们正在处理的当前值的个数
+        while(std::cin >> val) {    // 读取剩余的数
+            if(val == currVal) {    // 如果值相同
+                ++cnt;              // 将 cnt 加1
             }
-            else {
+            else {                  // 否则， 打印前一个值的个数
                 std::cout << currVal << " occurs " 
                           << cnt << " times " << std::endl;
-                currVal = val;
-                cnt = 1;
+                currVal = val;      // 记住新值
+                cnt = 1;            // 重置计数器
             }
         }
+        // 记住打印文件中最后一个值的个数
         std::cout << currVal << " occurs " 
                   << cnt << " times " << std::endl;
     }
-
     return 0;
 }
 
